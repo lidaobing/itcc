995a8c22a2d77962596d0f7b49f6dd9f21e95d10
nichloas <nichloas>
2005-01-27 05:36:46 +0000

    add class R6

--- src/CCS2/R6.py
+++ src/CCS2/R6.py
@@ -1,8 +1,11 @@
+# $Id$
 from itcc.CCS2.Mezei import R6
 from itcc.Tools.tools import length
+from itertools import chain
 
 
 __all__ = ['R6', 'R6_in_vivo']
+__revision__ = '$Rev$'
 
 def R6_in_vivo(points):
     assert len(points) == 7
@@ -12,6 +15,14 @@ def R6_in_vivo(points):
             for i in range(0,5)]
     return R6(points[:2] + points[-2:],
               len1, len2)
-    
-        
 
+class R6:
+    def __init__(self, data):
+        assert len(data) == 7
+        self.data = tuple(data)
+        
+    def kind(self):
+        return tuple([len(node) for node in self.data])
+
+    def needshakenodes(self):
+        return tuple(chain(*self.data[1:-1]))
