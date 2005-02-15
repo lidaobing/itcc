b618f5fb2880cf216a637ca43c85a3c3b5d8329f
nichloas <nichloas>
2005-02-15 03:06:27 +0000

    * src/CCS2/loopclosure.py: add `LoopClosure.shakehdata', this 	variable store the shake information for the `getr6result'

--- src/CCS2/loopclosure.py
+++ src/CCS2/loopclosure.py
@@ -5,10 +5,10 @@ import heapq
 import sets
 import pprint
 import itertools
-from copy import copy
+import time
 from itcc.Tinker import tinker
-from itcc.CCS2 import loopdetect, base, peptide, R6, Mezei
-from itcc.Molecule import read, write
+from itcc.CCS2 import loopdetect, base, peptide, R6, Mezei, shake
+from itcc.Molecule import read, write, tools as moltools
 from itcc.Tools import tools
 
 __all__ = ['LoopClosure']
@@ -18,63 +18,76 @@ class LoopClosure(object):
     def __init__(self, forcefield, keeprange, searchrange):
         self.forcefield = forcefield
         self.keeprange = keeprange
-        self.keepbound = None
         self.searchrange = searchrange
-        self.searchbound = None
-        self.maxsteps = 0
+        self.maxsteps = None
         self.eneerror = 0.0003
         self.moltypekey = None
         self.tasks = []
         self.taskheap = []
         self.enes = []
         self.minconverge = 0.001
-        
+        self.molnametmp = None
+        self.lowestene = None
+        self.step = 0
+        self.shakedata = None
+
+    def getkeepbound(self):
+        if self.keeprange is None:
+            return None
+        return self.lowestene + self.keeprange
+    keepbound = property(getkeepbound)
+
+    def getsearchbound(self):
+        if self.searchrange is None:
+            return None
+        return self.lowestene + self.searchrange
+    searchbound = property(getsearchbound)
+
     def __call__(self, molfname):
         self.printparams()
         mol = read.readxyz(file(molfname))
         self.molnametmp = os.path.splitext(molfname)[0] + '.%03i'
-        
-        moltype = getmoltype(self.moltypekey)(mol)
+        typedmol = getmoltype(self.moltypekey)(mol)
         self.loopatoms = self.getloopatoms(mol)
-        self.r6s = moltype.getr6s(self.loopatoms)
+        self.shakedata = shake.shakedata(mol, self.loopatoms)
+        self.r6s = typedmol.getr6s(self.loopatoms)
         printr6s(self.r6s)
-        self.combinations = moltype.getcombinations(self.r6s)
+        self.combinations = typedmol.getcombinations(self.r6s)
         self.r6s = tuple(sets.Set(itertools.chain(*self.combinations)))
         printcombinations(self.combinations)
-        
+
         mol, ene = tinker.minimizemol(mol, self.forcefield, self.minconverge)
-        self.addtask(mol, ene)
-        self._run(mol, ene)
-
-    def _run(self, mol, ene):
         self.lowestene = ene
-        self.updatebound()
-        step = 1
+        self.addtask(mol, ene)
+        for taskidx in self.taskqueue():
+            self.runtask(taskidx)
 
-        for taskidx, initmol, initene in self.taskqueue():
-            print
-            print ' CCS2 Local Search              Minimum %6i %21.4f' \
-                  % (taskidx + 1, initene)
-            print
-            
-            for cmbidx, idx, mol, ene in self.findneighbor(initmol):
-                print '  Step %5i Comb %2i %2i %44.4f' % \
-                      (step, cmbidx, idx, ene)
-                step += 1
-                if self.keepbound is not None:
-                    if ene > self.keepbound:
-                        continue
-                if self.isnewene(ene):
-                    self.addtask(mol, ene)
-                    if ene < self.lowestene:
-                        self.lowestene = ene
-                        self.updatebound()
-                        if initene > self.searchbound:
-                            break
-            print
+    def runtask(self, taskidx):
+        mol, ene = self.tasks[taskidx]
+        print
+        print ' CCS2 Local Search              Minimum %6i %21.4f' \
+              % (taskidx + 1, ene)
+        print
+
+        for newmol, newene in self.findneighbor(mol):
+            if self.isuseful(newene):
+                self.addtask(newmol, newene)
+            if newene < self.lowestene:
+                self.lowestene = newene
+                if ene >= self.searchbound:
+                    return
+
+    def isuseful(self, ene):
+        if not self.isnewene(ene):
+            return False
+        if self.keeprange is None or self.searchrange is None:
+            return True
+        if ene <= self.keepbound or ene <= self.searchbound:
+            return True
+        return False
 
     def addtask(self, mol, ene):
-        self.tasks.append(mol)
+        self.tasks.append((mol, ene))
         taskidx = len(self.tasks) - 1
         heapq.heappush(self.taskheap, (ene, taskidx))
         bisect.insort(self.enes, ene)
@@ -84,15 +97,13 @@ class LoopClosure(object):
         print
         self.writemol(taskidx+1, mol, ene)
 
-
     def taskqueue(self):
         while self.taskheap:
             ene, taskidx = heapq.heappop(self.taskheap)
             if self.searchbound is not None and ene > self.searchbound:
                 print self.searchbound
                 return
-            mol = self.tasks[taskidx]
-            yield taskidx, mol, ene
+            yield taskidx
 
     def getloopatoms(self, mol):
         loops = loopdetect.loopdetect(mol)
@@ -100,6 +111,7 @@ class LoopClosure(object):
         return loops[0]
 
     def printparams(self):
+        print 'Starttime: %s' % time.asctime()
         print 'Forcefield: %s' % self.forcefield
         print 'KeepRange: %s' % self.keeprange
         print 'SearchRange: %s' % self.searchrange
@@ -111,12 +123,6 @@ class LoopClosure(object):
         write.writexyz(mol, ofile, '%.4f' % ene)
         ofile.close()
 
-    def updatebound(self):
-        if self.searchrange is not None:
-            self.searchbound = self.lowestene + self.searchrange
-        if self.keeprange is not None:
-            self.keepbound = self.lowestene + self.keeprange
-
     def isnewene(self, ene):
         idx = bisect.bisect(self.enes, ene)
         if idx - 1 >= 0 and \
@@ -125,15 +131,15 @@ class LoopClosure(object):
         if idx < len(self.enes) and \
                abs(self.enes[idx] - ene) < self.eneerror:
             return False
+        bisect.insort(self.enes, ene)
         return True
 
     def findneighbor(self, mol):
-        mol.builddistancematrix()
         coords = mol.coords
-        dismat = mol.distancematrix
+        dismat = moltools.distmat(mol)
         r6results = {}
         for r6 in self.r6s:
-            r6results[r6] = getr6result(coords, r6, dismat)
+            r6results[r6] = getr6result(coords, r6, dismat, self.shakedata)
         for cmbidx, combine in enumerate(self.combinations):
             r6s = [r6results[r6] for r6 in combine]
             needshakeatoms = []
@@ -141,21 +147,23 @@ class LoopClosure(object):
                 needshakeatoms.extend(R6.R6(r6).needshakenodes())
             result = list(tools.combinecombine(r6s))
             for molidx, molresult in enumerate(result):
-                newmol = copy(mol)
+                newmol = mol.copy()
                 for r6result in molresult:
                     for idx, coord in r6result.items():
                         newmol.coords[idx] = coord
                 rmol, rene = tinker.minimizemol(newmol,
                                                 self.forcefield,
                                                 self.minconverge)
-                yield cmbidx, molidx, rmol, rene
+                self.step += 1
+                print '  Step %5i   Comb %02i-%02i %42.4f' % \
+                      (self.step, cmbidx, molidx, rene)
 
-                
+                yield rmol, rene
 
-def getr6result(coords, r6, dismat):
+def getr6result(coords, r6, dismat, shakedata):
     if r6type(r6) == (1,1,1,1,1,1,1):
         idxs = tuple(itertools.chain(*r6))
-        return Mezei.R6(coords, idxs, dismat)
+        return Mezei.R6(coords, idxs, dismat, shakedata)
     assert False, r6type(r6)
 
 def r6type(r6):
@@ -164,18 +172,14 @@ def r6type(r6):
 moltypedict = {'peptide': peptide.Peptide}
 def getmoltype(key):
     return moltypedict.get(key, base.Base)
-    
+
 def printr6s(r6s):
     print "This loop has %i R6 blocks:" % len(r6s)
     pprint.pprint(r6s)
     print
-    
+
 def printcombinations(combinations):
     print "These R6 blocks have %i kinds of combination:" % len(combinations)
     for i, combination in enumerate(combinations):
         print "Combination-%i: %s" % (i, combination)
     print
-            
-            
-            
-            
