9e29b5f7321660e94eae7056a0fef4511961f71d
nichloas <nichloas>
2005-03-06 12:48:06 +0000

    test/sidechain_test.py (main): add testsuite for sidechain src/CCS2/sidechain.py (movesidechain): add

--- src/CCS2/sidechain.py
+++ src/CCS2/sidechain.py
@@ -1,6 +1,7 @@
 # $Id$
 
 from itcc.Molecule import tools
+from itcc.CCS2 import pyramid
 
 __revision__ = '$Rev$'
 
@@ -20,3 +21,15 @@ def getsidechain(mol, loop, idx):
                 results.append(atomidx)
                 searchs.append(atomidx)
     return results
+
+def movesidechain(fromcoords, tocoords, refidxs, sidechain):
+    assert len(refidxs) == 3
+    reffrom = [fromcoords[idx] for idx in refidxs]
+    refto = [tocoords[idx] for idx in refidxs]
+    trans1, _ = pyramid.construct_both_transform_matrix(*reffrom)
+    _, trans2 = pyramid.construct_both_transform_matrix(*refto)
+    totaltrans = trans2 * trans1
+    result = {}
+    for idx in sidechain:
+        result[idx] = totaltrans(fromcoords[idx])
+    return result
---
+++ test/sidechain_test.py
@@ -0,0 +1,18 @@
+import unittest
+from itcc.Molecule import read
+from itcc.CCS2 import sidechain
+
+class TestSidechain(unittest.TestCase):
+    def testsidechain(self):
+        mol = read.readxyz(file('methane1.xyz'))
+        coords1 = mol.coords
+        coords2 = coords1[0:1] + coords1[3:] + coords1[1:3]
+        newcoords = sidechain.movesidechain(coords1, coords2, (0,1,2), (3,4))
+        self.assert_((newcoords[3] - coords2[3]).length() < 0.001)
+        self.assert_((newcoords[4] - coords2[4]).length() < 0.001)
+
+def main():
+    unittest.main()
+
+if __name__ == '__main__':
+    main()
