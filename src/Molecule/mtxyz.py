321418d90af44a13fbdbfd343ee5f377c636ad5d
nichloas <nichloas>
2005-07-17 02:41:07 +0000

    add mtxyz.py

---
+++ src/Molecule/mtxyz.py
@@ -0,0 +1,21 @@
+# $Id$
+
+'''Deal with multi tinker xyz file, such as TINKER .arc file.
+'''
+
+__revision__ = '$Rev$'
+
+from itcc.Molecule import read
+
+class Mtxyz:
+    def __init__(self, ifile):
+        self._ifile = ifile
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+            return read.readxyz(self._ifile)
+        except:
+            raise StopIteration
