3661e0508be73c9fdaafb726b0acd5c6c51a400c
nichloas <nichloas>
2005-08-06 01:11:58 +0000

    add another method

--- src/Tools/tor2freeene.py
+++ src/Tools/tor2freeene.py
@@ -1,5 +1,7 @@
 # $Id$
 import math
+import sys
+import getopt
 
 __revision__ = '$Rev$'
 
@@ -27,33 +29,81 @@ def tor2freeene(ifname, column, countnum, temperature):
     for i in range(len(counts)):
         print -180.0 + i * interval, counts[i], G[i]
 
+def tor2freeene2(ifname, column, countnum, temperature):
+    R = 8.314e-3 / 4.184                   # kcal/K*mol
+    datas = []
+
+    for line in file(ifname):
+        datas.append(float(line.split()[column]))
+
+    datas.sort()
+
+    assert len(datas) >= countnum
+
+    breaks = []
+
+    for idx in range(countnum+1):
+        breakidx = idx * len(datas) / countnum
+        if idx == 0:
+            breakvalue = datas[0]
+        elif idx == countnum:
+            breakvalue = datas[-1]
+        else:
+            breakvalue = (datas[breakidx-1]+datas[breakidx])/2.0
+        breaks.append(breakvalue)
+
+    lens = []
+    centers = []
+    for idx in range(countnum):
+        lens.append(breaks[idx+1] - breaks[idx])
+        centers.append((breaks[idx+1] + breaks[idx])/2.0)
+
+    minlen = min(lens)
+    for idx in range(countnum):
+        print centers[idx], R*temperature*math.log(lens[idx]/minlen)
+
+def usage(ofile=sys.stderr):
+    import os.path
+    print >> ofile, 'Usage: %s [-n] filename [column [count [temperature]]]' % \
+          os.path.basename(sys.argv[0])
+    print >> ofile
+    print >> ofile, '-n   new mode'
+    print >> ofile, 'default column is 1(first column)'
+    print >> ofile, 'default count is 36'
+    print >> ofile, 'default temperature is 300K'
+
 def main():
-    import sys
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hn", ["newmode", "help"])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
     count = 36
     temperature = 300.0
     column = 0
+    mode = tor2freeene
 
-    if not 2 <= len(sys.argv) <= 5:
-        import os.path
-        print >> sys.stderr, 'Usage: %s filename [column [count [temperature]]]' % \
-              os.path.basename(sys.argv[0])
-        print >> sys.stderr
-        print >> sys.stderr, 'default column is 1(first column)'
-        print >> sys.stderr, 'default count is %i' % count
-        print >> sys.stderr, 'default temperature is %.0fK' % temperature
-        sys.exit(1)
-    ifname = sys.argv[1]
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage(sys.stdout)
+            sys.exit()
+        elif o in ('-n', '--newmode'):
+            mode = tor2freeene2
 
-    if len(sys.argv) >= 3:
-        column = int(sys.argv[2]) - 1
-    if len(sys.argv) >= 4:
-        count = int(sys.argv[3])
-    if len(sys.argv) >= 5:
-        temperature = float(sys.argv[4])
+    if not 1 <= len(args) <= 4:
+        usage()
+        sys.exit(2)
+    ifname = args[0]
 
-    tor2freeene(ifname, column, count, temperature)
+    if len(args) >= 2:
+        column = int(args[1]) - 1
+    if len(args) >= 3:
+        count = int(args[2])
+    if len(args) >= 4:
+        temperature = float(args[3])
 
+    mode(ifname, column, count, temperature)
 
 if __name__ == '__main__':
     main()
