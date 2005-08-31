906fc0de303a7e6e6db91cb1fcc09cff7c088b11
nichloas <nichloas>
2005-08-31 07:01:31 +0000

    add onecolumn.py

---
+++ src/Tools/onecolumn.py
@@ -0,0 +1,14 @@
+def onecolumn(ifname):
+    for line in file(ifname):
+        for word in line.split():
+            print word
+
+def main():
+    import sys
+    if len(sys.argv) != 2:
+        sys.stderr.write('Usage: %s ifname\n' % sys.argv[0])
+        sys.exit(1)
+    onecolumn(sys.argv[1])
+
+if __name__ == '__main__':
+    main()
