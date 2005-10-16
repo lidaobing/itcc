3f758960d933ca86dadd58e2fa04f6150b0170b4
nichloas <nichloas>
2005-10-16 09:17:53 +0000

    new testsuite: dmddat2mtxyz

--- tests/rmsd_test.py
+++ tests/rmsd_test.py
@@ -3,9 +3,8 @@ from itcc.Molecule import _rmsd, read
 
 class TestRMSD(unittest.TestCase):
     def testrmsd(self):
-        mol1 = read.readxyz(file('cycC17.1.xyz'))
-        mol2 = read.readxyz(file('cycC17.2.xyz'))
-        print _rmsd.rmsd(mol1, mol2)
+        mol1 = read.readxyz(file('rmsd.1.xyz'))
+        mol2 = read.readxyz(file('rmsd.2.xyz'))
         self.assert_(abs(_rmsd.rmsd(mol1, mol2) - 1.001) < 0.001)
 
 def main():
--- tests/test.sh
+++ tests/test.sh
@@ -2,10 +2,13 @@
 
 set -ev
 
-python ../itcc/Tools/mol2top.py cycC17.1.xyz | \
-    diff mol2top.result -
+python ../itcc/Tools/mol2top.py mol2top.xyz | \
+    diff mol2top.ok -
 
-python ../itcc/Tools/onecolumn.py cycC17.1.xyz | \
-    diff onecolumn.result -
+python ../itcc/Tools/onecolumn.py onecolumn.xyz | \
+    diff onecolumn.ok -
+
+python ../itcc/Tools/dmddat2mtxyz.py -f 1-3/2,4-8/3 dmddat2mtxyz.dmddat dmddat2mtxyz.mol | \
+    diff dmddat2mtxyz.ok -
 
 echo OK
