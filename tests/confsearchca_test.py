6ac1629b890ffd1b8376bca3819ff8c94e2742b3
nichloas <nichloas>
2005-10-16 08:43:29 +0000

    update testsuite

--- tests/confsearchca_test.py
+++ tests/confsearchca_test.py
@@ -7,8 +7,8 @@ from itcc.CCS2 import sidechain
 class TestConfsearchCA(unittest.TestCase):
     def test(self):
         os.system('rm -f cycC9.*')
-        os.system('python2.4 makecyclicalkane.py 9 > cycC9.xyz')
-        self.assert_(os.system('confsearch.py -f mm3 cycC9.xyz > cycC9.log') == 0)
+        os.system('python makecyclicalkane.py 9 > cycC9.xyz')
+        self.assert_(os.system('itcc confsearch -f mm3 cycC9.xyz > cycC9.log') == 0)
         self.assert_(os.path.exists('cycC9.007'))
         self.assert_(not os.path.exists('cycC9.008'))
         os.system('rm -f cycC9.*')
