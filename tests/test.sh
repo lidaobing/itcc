0403d9e5c75f42e6640fce17e5d4f10f2988ef11
nichloas <nichloas>
2005-12-15 07:52:54 +0000

    update testsuite

--- tests/test.sh
+++ tests/test.sh
@@ -8,11 +8,11 @@ python ../itcc/Tools/mol2top.py mol2top.xyz | \
 python ../itcc/Tools/onecolumn.py onecolumn.xyz | \
     diff onecolumn.ok -
 
-#python ../itcc/Tools/dmddat2mtxyz.py -f 1-3/2,4-8/3 dmddat2mtxyz.dmddat dmddat2mtxyz.mol | \
-#    diff dmddat2mtxyz.ok -
+python ../itcc/Tools/dmddat2mtxyz.py -f 1-3/2,4-8/3 dmddat2mtxyz.dmddat dmddat2mtxyz.mol | \
+    diff dmddat2mtxyz.ok -
 
-#python ../itcc/Tools/dmddat2xtc.py dmddat2xtc.dmddat dmddat2xtc.tmp.xtc
-#cmp dmddat2xtc.tmp.xtc dmddat2xtc.`uname -m`.ok.xtc
-#rm -f dmddat2xtc.tmp.xtc
+python ../itcc/Tools/dmddat2xtc.py dmddat2xtc.dmddat dmddat2xtc.tmp.xtc
+cmp dmddat2xtc.tmp.xtc dmddat2xtc.`uname -m`.ok.xtc
+rm -f dmddat2xtc.tmp.xtc
 
 echo OK
