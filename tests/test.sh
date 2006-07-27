d0433a63ab95de050ae83e48787c14c9bf194616
nichloas <nichloas>
2006-07-27 14:00:12 +0000

    move

--- tests/test.sh
+++ tests/test.sh
@@ -4,16 +4,16 @@ set -e
 
 PYTHON=python2.4
 
-$PYTHON ../itcc/tools/mol2top.py mol2top.xyz | \
+$PYTHON ../itcc/molecule/mol2top.py mol2top.xyz | \
     diff mol2top.ok -
 
 $PYTHON ../itcc/tools/onecolumn.py onecolumn.xyz | \
     diff onecolumn.ok -
 
-$PYTHON ../itcc/tools/dmddat2mtxyz.py -f 1-3/2,4-8/3 dmddat2mtxyz.dmddat dmddat2mtxyz.mol | \
+$PYTHON ../itcc/molecule/dmddat2mtxyz.py -f 1-3/2,4-8/3 dmddat2mtxyz.dmddat dmddat2mtxyz.mol | \
     diff dmddat2mtxyz.ok -
 
-$PYTHON ../itcc/tools/xyz2pdb.py xyz2pdb.xyz - \
+$PYTHON ../itcc/molecule/xyz2pdb.py xyz2pdb.xyz - \
     | diff xyz2pdb.ok -
 
 echo OK
