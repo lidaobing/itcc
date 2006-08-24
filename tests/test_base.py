f4a7430762ae1d2cd2da699fe6734633f35eda31
nichloas <nichloas>
2006-08-24 11:56:11 +0000

    confirm the build directory exist

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,6 +2,10 @@ import sys
 import os.path
 import distutils.util
 
-sys.path.insert(0,
-                os.path.abspath('../build/lib.%s-%s' %
-                    (distutils.util.get_platform(), sys.version[0:3])))
+path = os.path.abspath('../build/lib.%s-%s' %
+                         (distutils.util.get_platform(), sys.version[0:3]))
+if not os.path.exists(path):
+    sys.stderr.write('please build first\n')
+    sys.exit(1)
+
+sys.path.insert(0, path)
