b29e88707760907cd9012df889ba87ea2459eb12
nichloas <nichloas>
2006-08-24 14:48:14 +0000

    update

--- tests/test.py
+++ tests/test.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2.4
 import test_base
-from confsearch_test import TestConfsearch
-from rmsd_test import TestRMSD
-from sidechain_test import TestSidechain
+from test_confsearch import TestConfsearch
+from test_rmsd import TestRMSD
+from test_sidechain import TestSidechain
 from test_xyz2pdb import TestXyz2pdb
 from test_mol2top import *
 from test_onecolumn import *
