b2f7cbd152c91477b068652cc391ca969f5bc83b
nichloas <nichloas>
2006-08-24 14:29:37 +0000

    reformat dmddat2mtxyz testcase

--- tests/test.py
+++ tests/test.py
@@ -6,6 +6,7 @@ from sidechain_test import TestSidechain
 from test_xyz2pdb import TestXyz2pdb
 from test_mol2top import *
 from test_onecolumn import *
+from test_dmddat2mtxyz import *
 import unittest
 
 if __name__ == '__main__':
