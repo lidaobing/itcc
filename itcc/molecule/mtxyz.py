190e8417af743939df6d0178cbeef50d196b07ba
nichloas <nichloas>
2006-12-30 05:56:46 +0000

    add new funtcion for mtxyz

--- itcc/molecule/mtxyz.py
+++ itcc/molecule/mtxyz.py
@@ -19,3 +19,22 @@ class Mtxyz:
             return read.readxyz(self._ifile)
         except:
             raise StopIteration
+
+    def read_mol_as_string(self):
+        result = ""
+        cur = 0
+        need = None
+        for line in self._ifile:
+            if cur == 0:
+                need = int(line.split()[0]) + 1
+            result += line
+            cur += 1
+            if cur == need:
+                yield result
+                result = ""
+                cur = 0
+                need = None
+
+            
+
+        
