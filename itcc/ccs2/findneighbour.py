8c08c561a5262b8e22830dd59388eadcd3e79dc9
nichloas <nichloas>
2007-01-05 13:51:20 +0000

    add findneighbour

---
+++ itcc/ccs2/findneighbour.py
@@ -0,0 +1,24 @@
+# $Id$
+
+import sys
+import pprint
+
+from itcc.molecule import read
+from itcc.ccs2 import loopdetect, loopclosure, peptide
+
+def main():
+    mol = read.readxyz(file(sys.argv[1]))
+    loop  = loopdetect.loopdetect(mol)[0]
+    loopc = loopclosure.LoopClosure()
+
+    shakedata = loopclosure.getshakedata(mol, loop)
+    loopc.shakedata = shakedata
+    loopc.forcefield = "oplsaa"
+    loopc.log_level = 0
+
+    for r6 in peptide.Peptide(mol).getr6s(loop):
+        for mol, ene in loopc.findneighbor(mol, r6):
+            print ene
+
+if __name__ == '__main__':
+    main()
