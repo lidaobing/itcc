0c72dd7dc358d350eeb81c238f75ee1f3c52daca
nichloas <nichloas>
2007-01-13 01:21:28 +0000

    log2enestep

---
+++ itcc/ccs2/log2enestep.py
@@ -0,0 +1,21 @@
+# $Id$
+
+def log2enestep(ifile, ofile):
+    step = 0
+    for line in ifile:
+        if line.startswith("  Step"):
+            step = int(line.split()[1])
+        elif line.startswith("    Potential"):
+            ofile.write("%s %s\n" % (line.split()[-1], step))
+
+def main():
+    import sys
+    if len(sys.argv) != 2:
+        import os.path
+        print 'usage: %s ifile > ofile' % os.path.basenaem(sys.argv[0])
+        sys.exit(1)
+    log2enestep(file(sys.argv[1]), sys.stdout)    
+
+
+if __name__ == '__main__':
+    main()
