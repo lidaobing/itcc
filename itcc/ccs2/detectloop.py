4b7a0c86d8a7e5de5756344ae79a29dfc22a6db7
lidaobing@gmail.com <lidaobing@gmail.com>
2007-07-15 02:30:29 +0000

    * itcc/ccs2/detectloop.py: new algorithm support H11C6-C6H11.

--- itcc/ccs2/detectloop.py
+++ itcc/ccs2/detectloop.py
@@ -14,6 +14,102 @@ NOLOOP = 0
 SIMPLELOOPS = 1
 COMPLEXLOOPS = 2
 
+def part_loop(loop):
+    res = []
+    cache = set()
+    while len(cache) < len(loop):
+        k = (set(loop.keys()) - cache).pop()
+        res.append(set([k]))
+        c2 = res[-1]
+        task = [k]
+        
+        while task:
+            k = task.pop()
+            for x in loop[k]:
+                if x in c2:
+                    continue
+                c2.add(x)
+                task.append(x)
+        cache |= c2
+    return res
+
+def delleaf(loop):
+    while 1:
+        ready = True
+        for k, v in loop.items():
+            if len(v) == 1:
+                loop[v.pop()].remove(k)
+                del loop[k]
+                ready = False
+                break
+        if ready:
+            break
+
+def split_loop(loop):
+    delleaf(loop)
+    
+    for i1, i2s in loop.items():
+        for i2 in i2s:
+            if i1 >= i2:
+                continue
+            if len(i2s) == 2 and len(loop[i2]) == 2:
+                continue
+            loop2 = loop.copy()
+            loop2[i1] = set(loop[i1])
+            loop2[i2] = set(loop[i2])
+            loop2[i1].remove(i2)
+            loop2[i2].remove(i1)
+            parts = part_loop(loop2)
+            if len(parts) > 1:
+                res = []
+                for part in parts:
+                    partloop = {}
+                    for x in part:
+                        partloop[x] = loop2[x]
+                    res.extend(split_loop(partloop))
+                return res
+            
+    if not loop:
+        return []
+    
+    max_deg = max([len(v) for v in loop.values()])
+    if max_deg == 2:
+        res = []
+        res.append(min(loop.keys()))
+        res.append(min(loop[res[0]]))
+        while len(res) < len(loop):
+            res.append([x for x in loop[res[-1]] if x != res[-2]][0])
+        return [res]
+    
+    tasks = set()
+    for k, v in loop.items():
+        if len(v) > 2:
+            for x in v:
+                tasks.add((k, x))
+    
+    res = []
+    while tasks:
+        task = tasks.pop()
+        res.append(list(task))
+        while len(loop(task[-1])) == 2:
+            res.append([x for x in loop[task[-1]] if x != task[-2]][0])
+        tasks.remove(tuple(res[-1:-3:-1]))
+        res[-1] = min(res[-1], res[-1][::-1])
+    return [res]
+
+def loopdetect2(mol):
+    loop = {}
+    for i in range(len(mol)):
+        loop[i] = set()
+    
+    for i in range(len(mol)):
+        for j in range(i):
+            if mol.is_connect(i, j):
+                loop[i].add(j)
+                loop[j].add(i)
+    
+    return split_loop(loop)
+
 def _symmatp(mat):
     """_symmatp(mat) -> Bool
     if mat[i][j] == mat[j][i]:
@@ -148,17 +244,21 @@ def main():
         sys.stderr.write('Usage: %s xyzfname\n' % os.path.basename(sys.argv[0]))
         sys.exit(1)
     from itcc.molecule import read
-    res = loopdetect(read.readxyz(file(sys.argv[1])))
-    if res[0] == NOLOOP:
+    res = loopdetect2(read.readxyz(file(sys.argv[1])))
+    if not res:
         print 'NOLOOP'
-    elif res[0] == SIMPLELOOPS:
-        print 'SIMPLELOOPS'
-        for loop in res[1]:
-            print ' '.join([str(x+1) for x in std_simple_loop(loop)])
-    else:
-        print 'COMPLEXLOOPS'
-        for loop in res[1]:
-            print ' '.join([str(x+1) for x in std_complex_loop(loop)])
+    for x in res:
+        try:
+            if isinstance(x[0], int):
+                print 'SIMPLELOOPS'
+                print ' ' + ' '.join([str(y+1) for y in x])
+            else:
+                print 'COMPLEXLOOPS'
+                for x2 in x:
+                    print ' ' + ' '.join([str(y+1) for y in x2])
+        except:
+            print `x`
+            raise
 
 if __name__ == '__main__':
     main()
