2273f3f036f9887cf11e397a811bafbc15d0044c
lidaobing@gmail.com <lidaobing@gmail.com>
2007-07-18 14:39:32 +0000

    * itcc/ccs2/catordiff.py: use itcc.ccs2.tordiff module.

--- itcc/ccs2/catordiff.py
+++ itcc/ccs2/catordiff.py
@@ -3,11 +3,10 @@
 
 import sys
 import math
-import logging
 
 from itcc.tools import periodnumber
 from itcc.molecule import read, molecule, tools
-from itcc.ccs2 import detectloop
+from itcc.ccs2 import detectloop, tordiff
 
 __revision__ = '$Rev$'
 __all__ = ['catordiff']
@@ -21,7 +20,7 @@ def catordiff(mol1, mol2, loop=None):
     assert isinstance(mol2, molecule.Molecule)
     tors1 = getlooptor(mol1, loop)
     tors2 = getlooptor(mol2, loop)
-    return tordiff(tors1, tors2)
+    return tordiff.torsdiff(tors1, tors2, True, 0, 1)
 
 def getlooptor(mol, loop):
     if loop is None:
@@ -29,29 +28,6 @@ def getlooptor(mol, loop):
         assert len(loops) == 1
         loop = loops[0]
     return tools.calclooptor(mol, loop)
-
-# TODO: need a testcase
-def tordiff(tors1, tors2):
-    pi = math.pi
-    result = 2 * pi
-    n = len(tors1)
-    for newtors2 in varytors(tors2):
-        thisresult = 0.0
-        for i in range(n):
-            thisresult = max(thisresult, pi - abs(abs(tors1[i] - newtors2[i]) - pi))
-            if thisresult > result:
-                break
-            
-        logging.debug("%f" % thisresult)
-        result = min(result, thisresult)
-    return result
-
-def varytors(tors):
-    negtors = [-tor for tor in tors]
-    vartors = [tors * 2, tors[::-1]*2, negtors*2, negtors[::-1]*2]
-    for subvartors in vartors:
-        for idx in range(len(tors)):
-            yield subvartors[idx:idx+len(tors)]
 
 def main():
     if len(sys.argv) != 3:
