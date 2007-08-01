3ea8da0cda69d6c6f4c2ab56551f7710454a0cc0
lidaobing@gmail.com <lidaobing@gmail.com>
2007-08-01 20:25:37 +0000

    add itcc-mtxyz2txyz

---
+++ itcc/tools/mtxyz2txyz.py
@@ -0,0 +1,29 @@
+# $Id$
+
+from itcc.molecule import read, write
+
+def mtxyz2xyz(ifile, ofilenamepattern):
+    i = 1
+    while 1:
+        try:
+            ofname = ofilenamepattern % i
+            mol = read.readxyz(ifile)
+            write.writexyz(mol, file(ofname, 'w'))
+        except:
+            break
+def main():
+    import sys
+    if len(sys.argv) != 3:
+        import os.path
+        sys.stderr.write('Usage: %s <ifile|-> ofilenamepattern\n' % os.path.basename(sys.argv[0]))
+        sys.stderr.write('Example: %s foo.mtxyz foo-%%03i.xyz\n' % os.path.basename(sys.argv[0]))
+        sys.exit(1)
+    
+    ifile = sys.stdin
+    if sys.argv[1] != '-':
+        ifile = file(sys.argv[1])
+        
+    mtxyz2txyz(ifile, sys.argv[2])
+
+if __name__ == '__main__':
+    main()
