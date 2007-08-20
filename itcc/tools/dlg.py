183c44413c71b96122af5ae539064d8c6d98b24b
lidaobing@gmail.com <lidaobing@gmail.com>
2007-08-20 19:54:55 +0000

    * itcc/tools/dlg.py: new class Dlg

---
+++ itcc/tools/dlg.py
@@ -0,0 +1,51 @@
+# $Id$
+
+class DlgItem(object):
+    def __init__(self, ifile):
+        self.good = False
+        self.rank = None
+        for line in ifile:
+            if line.startswith('USER    Cluster Rank ='):
+                self.rank = int(line.split()[-1])
+                break
+            
+        if rank is None:
+            return
+        
+        for line in ifile:
+            if line.startswith('USER    RMSD from reference structure       ='):
+                self.rmsd = float(line.split()[-2])
+                break
+        
+        for line in ifile:
+            if line.startswith('USER    Estimated Free Energy of Binding    ='):
+                self.ene = float(line.split()[-3])
+                break
+            
+        self.mol = ''
+        for line in ifile:
+            if line.split()[:1] == ['ATOM']:
+                self.mol += self.line
+            elif line.split()[:1] == ['TER']:
+                break
+        
+        self.good = True
+        
+
+    def __nonzero__(self):
+        return self.good
+        
+
+class Dlg(object):
+    def __init__(self, ifile):
+        self.ifile = ifile
+        
+    def __iter__(self):
+        return self
+    
+    def next(self):
+        res = DlgItem(self.ifile)
+        if res:
+            yield res
+        else:
+            raise StopIteration
