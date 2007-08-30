f087edcc48e3d89e20a1d9c1f038366a52c9772c
lidaobing@gmail.com <lidaobing@gmail.com>
2007-08-30 08:04:18 +0000

    itcc/tools/ccslog2enestep.py: itcc-ccslog2enestep does not work.

--- itcc/tools/ccslog2enestep.py
+++ itcc/tools/ccslog2enestep.py
@@ -1,6 +1,6 @@
 # $Id$
 
-def main(ifile, ofile):
+def ccslog2enestep(ifile, ofile):
     step = 0
     for line in ifile:
         if line.startswith("  Step"):
@@ -8,10 +8,14 @@ def main(ifile, ofile):
         elif line.startswith("    Potential"):
             ofile.write("%s %s\n" % (line.split()[-1], step))
 
-if __name__ == '__main__':
+def main():
     import sys
     if len(sys.argv) != 2:
         import os.path
         sys.stderr.write('Usage: %s ifile > ofile\n' % os.path.basename(sys.argv[0]))
         sys.exit(1)
-    main(file(sys.argv[1]), sys.stdout)    
+    ccslog2enestep(file(sys.argv[1]), sys.stdout)    
+
+if __name__ == '__main__':
+    main()
+    
