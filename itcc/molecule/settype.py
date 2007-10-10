b4aac44ed0b56f307aada31b7e35f45b03516246
lidaobing@gmail.com <lidaobing@gmail.com>
2007-10-10 01:10:15 +0000

    itcc/molecule/settype.py: support multi input xyz file.

--- itcc/molecule/settype.py
+++ itcc/molecule/settype.py
@@ -17,23 +17,35 @@ def settype(xyzfile, typefile):
     for idx, atype in enumerate(types):
         mol.settype(idx, atype)
     write.writexyz(mol, sys.stdout)
-
+    
+def settype2(xyzfile, types, ofile):
+    line = xyzfile.readline()
+    assert int(line.split()[0]) == len(types)
+    
+    ofile.write(line)
+    for t in types:
+        words = xyzfile.readline().split()
+        words[5] = t
+        ofile.write(' '.join(words) + '\n')
+    
 def main():
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3:
         import os.path
-        print >> sys.stderr, "Usage: %s {XYZFNAME|-} {TYPEFNAME|-}" % \
+        print >> sys.stderr, "Usage: %s {TYPEFNAME|-} {XYZFNAME|-}..." % \
               os.path.basename(sys.argv[0])
         sys.exit(1)
-    if sys.argv[1] == '-':
-        xyzfile = sys.stdin
-    else:
-        xyzfile = file(sys.argv[1])
 
-    if sys.argv[2] == '-':
+    if sys.argv[1] == '-':
         typefile = sys.stdin
     else:
-        typefile = file(sys.argv[2])
-    settype(xyzfile, typefile)
+        typefile = file(sys.argv[1])
+        
+    types = typefile.read().split()
+    for fname in sys.argv[2:]:
+        ifile = sys.stdin
+        if fname != '-':
+            ifile = file(fname)
+        settype2(ifile, types, sys.stdout)
 
 if __name__ == '__main__':
     main()
